a85b182f97593b850c78d1c675fce720c9f42bf0
MarRue <20143778+gpluscb@users.noreply.github.com>
2022-03-30 12:12:26 +0200

    More progress on engine Implement player_rating and closing of elapsed rating periods

--- src/engine.rs
+++ src/engine.rs
@@ -1,14 +1,12 @@
 use std::time::{Duration, Instant};
 
-use crate::algorithm::{RatingResult, ScaledPlayerResult, Score};
+use crate::algorithm::{self, RatingResult, ScaledPlayerResult, Score};
 use crate::model::{Parameters, ScaledRating};
 use crate::util::PushOnlyVec;
 
 pub struct ScaledPlayer {
-    last_active: Option<Instant>, // TODO: Maybe last_active_rating_period?
     rating: ScaledRating,
-    // PushOnlyVec because we only push.
-    current_rating_period_results: PushOnlyVec<ScaledPlayerResult>,
+    current_rating_period_results: Vec<ScaledPlayerResult>,
 }
 
 // In this case, just engine::Rating does not tell enough about the purpose of the struct in my opinion.
@@ -38,20 +36,23 @@ impl RatingEngine {
         let index = self.managed_players.vec().len();
 
         self.managed_players.push(ScaledPlayer {
-            last_active: None,
             rating,
-            current_rating_period_results: PushOnlyVec::new(),
+            current_rating_period_results: Vec::new(),
         });
 
         index
     }
 
+    /// Registers a result in the current rating period.
+    ///
     /// # Panics
     ///
     /// This function might panic if the `result`'s players do not come from this `RatingEngine`.
     pub fn register_result<S: Score>(&mut self, result: &RatingResult<S>) {
-        // FIXME: Check if rating period should close
+        // We have to maybe close so the results will be added in the right rating period.
+        self.maybe_close_rating_periods_now();
 
+        // Split the result into two ScaledPlayerResults and save that on the players
         let player_1_rating = self
             .managed_players
             .vec()
@@ -85,9 +86,58 @@ impl RatingEngine {
             ));
     }
 
+    #[must_use]
     pub fn player_rating(&mut self, player_idx: usize) -> ScaledRating {
-        // FIXME: Check if rating period should close
+        let elapsed_periods = self.maybe_close_rating_periods_now();
 
-        todo!() // TODO
+        let player = self
+            .managed_players
+            .vec()
+            .get(player_idx)
+            .expect("Player index didn't belong to this RatingEngine");
+
+        algorithm::rate_player(
+            player.rating,
+            &player.current_rating_period_results,
+            elapsed_periods,
+            self.parameters,
+        )
+    }
+
+    /// # Returns
+    ///
+    /// The elapsed periods in the current rating period *after* all previous periods have been closed.
+    fn maybe_close_rating_periods_now(&mut self) -> f64 {
+        let elapsed_periods = self.elapsed_periods();
+
+        // We won't have negative elapsed_periods. Truncation this is the wanted result.
+        #[allow(clippy::cast_possible_truncation, clippy::cast_sign_loss)]
+        let periods_to_close = elapsed_periods as u32;
+
+        // Every result is in the first rating period that needs to be closed.
+        // This is guaranteed because we call this method before every time a new result gets added.
+        for player in self.managed_players.iter_mut() {
+            for _ in 0..periods_to_close {
+                algorithm::close_player_rating_period(
+                    &mut player.rating,
+                    &player.current_rating_period_results,
+                    self.parameters,
+                );
+
+                // We have now submitted the results to the players rating
+                player.current_rating_period_results.clear();
+            }
+        }
+
+        self.last_rating_period_start += periods_to_close * self.rating_period_duration;
+
+        elapsed_periods.fract()
+    }
+
+    #[must_use]
+    pub fn elapsed_periods(&self) -> f64 {
+        let elapsed_duration = self.last_rating_period_start.elapsed();
+
+        elapsed_duration.as_secs_f64() / self.rating_period_duration.as_secs_f64()
     }
 }
