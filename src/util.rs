05e3654306cb97a1c20f5c087519823e44d2f106
MarRue <20143778+gpluscb@users.noreply.github.com>
2022-03-30 12:47:58 +0200

    False suggestion by rust-analyzer

--- src/util.rs
+++ src/util.rs
@@ -1,5 +1,10 @@
+//! Utilities that are needed or useful for the rest of the crate,
+//! but that don't have anything really to do with the core of the crate.
+
 use std::slice::IterMut;
 
+/// A wrapper around [`Vec`] that only let's you append.
+/// This allows for indic into the [`Vec`] to always stay valid.
 #[derive(Clone, Default, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
 pub struct PushOnlyVec<T>(Vec<T>);
 
@@ -16,16 +21,22 @@ impl<T> From<PushOnlyVec<T>> for Vec<T> {
 }
 
 impl<T> PushOnlyVec<T> {
+    /// Creates a new, empty `PushOnlyVec<T>`.
+    ///
+    /// See also: [`Vec::new()`]
     #[must_use]
     pub fn new() -> Self {
         PushOnlyVec(Vec::new())
     }
 
+    /// Returns an immutable reference to the underlying [`Vec`].
+    /// Use this for all immutable operations on the [`Vec`].
     #[must_use]
     pub fn vec(&self) -> &Vec<T> {
         &self.0
     }
 
+    /// Appends an element to the
     pub fn push(&mut self, value: T) {
         self.0.push(value);
     }
