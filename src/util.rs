1c1b33887a464035ba35282d3c37727e7f7eb888
MarRue <20143778+gpluscb@users.noreply.github.com>
2022-04-06 20:23:09 +0200

    Make serde attributes more clean

--- src/util.rs
+++ src/util.rs
@@ -10,10 +10,12 @@ use serde::{Deserialize, Serialize};
 /// This allows for indic into the [`Vec`] to always stay valid.
 #[derive(Clone, Default, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
-#[cfg_attr(feature = "serde", serde(transparent))]
 #[cfg_attr(
     feature = "serde",
-    serde(bound(serialize = "T: Serialize", deserialize = "T: Deserialize<'de>"))
+    serde(
+        transparent,
+        bound(serialize = "T: Serialize", deserialize = "T: Deserialize<'de>")
+    )
 )]
 pub struct PushOnlyVec<T>(Vec<T>);
 
